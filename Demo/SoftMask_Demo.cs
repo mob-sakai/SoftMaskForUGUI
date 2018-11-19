7c8981af2fe67dd9b14217f9dd8524b2e142db55
mob-sakai <sakai861104@gmail.com>
2018-11-20 08:49:27 +0900

    Add demo

---
+++ Demo/SoftMask_Demo.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Coffee.UIExtensions.Demos
+{
+	public class SoftMask_Demo : MonoBehaviour
+	{
+		[SerializeField] RawImage softMaskBufferViewer;
+		[SerializeField] SoftMask softMask;
+
+
+		// Use this for initialization
+		void OnEnable()
+		{
+			softMaskBufferViewer.texture = softMask.softMaskBuffer;
+		}
+	
+		// Update is called once per frame
+		void Update()
+		{
+		
+		}
+
+		public void SetWorldSpase(bool flag)
+		{
+			GetComponent<Canvas>().renderMode = flag ? RenderMode.WorldSpace : RenderMode.ScreenSpaceCamera;
+			transform.rotation = flag ? Quaternion.Euler(new Vector3(0, 6, 0)) : Quaternion.identity;
+		}
+	}
+}
