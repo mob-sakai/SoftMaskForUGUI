87b60600b5608a54a58c2e8d23e73818dc15ca31
mob-sakai <sakai861104@gmail.com>
2023-12-31 20:34:24 +0900

    feat: exclude unused shader variants from the build (option)

---
+++ Packages/src/Editor/ShaderPreProcessor.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using Coffee.UISoftMask.Internal;
+using UnityEditor.Rendering;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace Coffee.UISoftMask
+{
+    internal class ShaderPreProcessor : ShaderPreProcessorBase
+    {
+        private static readonly string[] s_IgnoredKeywords =
+        {
+            "UNITY_UI_ALPHACLIP",
+            "UNITY_UI_CLIP_RECT"
+        };
+
+        public override int callbackOrder => 1;
+
+        public override void OnProcessShader(Shader shader, ShaderSnippetData snippet,
+            IList<ShaderCompilerData> data)
+        {
+            // If the shader is not SoftMask/softMaskable, do nothing.
+            var type = GetShaderType(shader);
+            if (type == ShaderType.None) return;
+
+            // Remove the 'UI_SOFT_MASKABLE_EDITOR' shader variants.
+            var editor = new ShaderKeyword(shader, "UI_SOFT_MASKABLE_EDITOR");
+            StripUnusedVariantsIf(data, d => d.shaderKeywordSet.IsEnabled(editor));
+
+            // If the shader is separated soft-maskable, remove non-soft-maskable variants.
+            if (type == ShaderType.SeparatedSoftMaskable)
+            {
+                var softMaskable = new ShaderKeyword(shader, "UI_SOFT_MASKABLE");
+                StripUnusedVariantsIf(data, d => !d.shaderKeywordSet.IsEnabled(softMaskable));
+            }
+
+            // If strip shader variants is disabled in the project, do nothing.
+            if (!UISoftMaskProjectSettings.instance.m_StripShaderVariants) return;
+
+            // If soft mask is disabled in the project, remove the all shader variants.
+            var softMaskDisabled = !UISoftMaskProjectSettings.instance.m_SoftMaskEnabled;
+            if (softMaskDisabled)
+            {
+                StripUnusedVariantsIf(data, true);
+                return;
+            }
+
+            // If stereo is disabled in the project, remove the 'UI_SOFT_MASKABLE_STEREO' shader variants.
+            if (!UISoftMaskProjectSettings.stereoEnabled)
+            {
+                var stereo = new ShaderKeyword(shader, "UI_SOFT_MASKABLE_STEREO");
+                StripUnusedVariantsIf(data, d => d.shaderKeywordSet.IsEnabled(stereo));
+            }
+
+            // Log
+            if (snippet.shaderType == UnityEditor.Rendering.ShaderType.Fragment)
+            {
+                Log(shader, data, s_IgnoredKeywords);
+            }
+        }
+
+        private static ShaderType GetShaderType(Shader shader)
+        {
+            if (!shader) return ShaderType.None;
+            var name = shader.name;
+            if (name == "Hidden/UI/SoftMask") return ShaderType.SoftMask;
+            if (!name.EndsWith(" (SoftMaskable)")) return ShaderType.None;
+            return name.StartsWith("Hidden/")
+                ? ShaderType.SeparatedSoftMaskable
+                : ShaderType.HybridSoftMaskable;
+        }
+
+        private enum ShaderType
+        {
+            None,
+            SoftMask,
+            HybridSoftMaskable,
+            SeparatedSoftMaskable
+        }
+    }
+}
