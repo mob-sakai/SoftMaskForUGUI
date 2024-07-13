9d8b5a133d9f80eb1f355ea99b47784facad30c5
mob-sakai <12690315+mob-sakai@users.noreply.github.com>
2024-07-13 11:58:11 +0900

    chore: Automatically import TMP support on load (development)

--- tmp-support-stable.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-# Switch the shaders for TextMeshPro (stable)
-target="TextMeshPro Support"
-sample="TextMeshPro Support"
-[ -d "Assets/$target" ] && rm -rf "Assets/$target"
-ln -s "../Packages/src/Samples~/$sample~" Assets
-mv "Assets/$sample~" "Assets/$target"
--- tmp-support-v3.2-or-v4.0.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-# Switch the shaders for TextMeshPro (v3.2 or v4.0)
-target="TextMeshPro Support"
-sample="TextMeshPro Support v3.2 or v4.0"
-[ -d "Assets/$target" ] && rm -rf "Assets/$target"
-ln -s "../Packages/src/Samples~/$sample~" Assets
-mv "Assets/$sample~" "Assets/$target"
